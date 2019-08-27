3c0b38c75809695bf238308dca9774b18c0cd316
boywaygl <betamditthui@gmail.com>
2019-08-27 14:31:11 +0700

    up2

--- app/src/main/java/com/example/student/bt2_1/MainActivity.java
+++ app/src/main/java/com/example/student/bt2_1/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.student.bt2_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void btntong(View v){
+        EditText edtsoA = (EditText)findViewById(R.id.edtsoA);
+        int a = Integer.parseInt(edtsoA.getText()+"");
+        EditText edtsoB = (EditText)findViewById(R.id.edtsoB);
+        int b = Integer.parseInt(edtsoB.getText()+"");
+        TextView txt = (TextView)findViewById(R.id.tv1);
+        txt.setText((a+b)+"");
+    }
+    public void btnhieu(View v){
+        EditText edtsoA = (EditText)findViewById(R.id.edtsoA);
+        int a = Integer.parseInt(edtsoA.getText()+"");
+        EditText edtsoB = (EditText)findViewById(R.id.edtsoB);
+        int b = Integer.parseInt(edtsoB.getText()+"");
+        TextView txt = (TextView)findViewById(R.id.tv1);
+        txt.setText((a-b)+"");
+    }
+    public void btntich(View v){
+        EditText edtsoA = (EditText)findViewById(R.id.edtsoA);
+        int a = Integer.parseInt(edtsoA.getText()+"");
+        EditText edtsoB = (EditText)findViewById(R.id.edtsoB);
+        int b = Integer.parseInt(edtsoB.getText()+"");
+        TextView txt = (TextView)findViewById(R.id.tv1);
+        txt.setText((a*b)+"");
+    }
+    public void btnthuong(View v){
+        EditText edtsoA = (EditText)findViewById(R.id.edtsoA);
+        int a = Integer.parseInt(edtsoA.getText()+"");
+        EditText edtsoB = (EditText)findViewById(R.id.edtsoB);
+        int b = Integer.parseInt(edtsoB.getText()+"");
+        TextView txt = (TextView)findViewById(R.id.tv1);
+        txt.setText((a/b)+"");
+    }
+    public void btnexit(View v){
+        finish();}
 }
